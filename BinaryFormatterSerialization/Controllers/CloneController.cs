533a2f8948bc6deeb4c186fc875d460b8f3591f1
Damir Arh <damir.arh@gmail.com>
2023-08-06 09:18:21 +0200

    Add project files.

---
+++ BinaryFormatterSerialization/Controllers/CloneController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace BinaryFormatterSerialization.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CloneController : ControllerBase
+    {
+        [HttpPost]
+        public Person Post(Person person)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                // Serialize the object into the memory stream.
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(memoryStream, person);
+
+                // Rewind the stream and use it to create a new object.
+                memoryStream.Position = 0;
+                return (Person)formatter.Deserialize(memoryStream);
+            }
+        }
+    }
+}
---
+++ BinaryFormatterSerialization/Person.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace BinaryFormatterSerialization
+{
+    [Serializable]
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
